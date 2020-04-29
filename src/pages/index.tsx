da8050473a9e6615b53ece7e3164420d248f80e9
Laoliu <Laoliu@lanmv.com>
2020-04-29 09:04:07 +0800

    Cache uri, params

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,55 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
 require('codemirror/mode/javascript/javascript');
 require('codemirror/addon/hint/show-hint');
 require('codemirror/addon/hint/javascript-hint');
 
-import { Row, Col, Input, Divider, Button, Tree, notification, Card, Typography, Radio } from 'antd';
+import {
+  Row,
+  Col,
+  Input,
+  Divider,
+  Button,
+  Tree,
+  notification,
+  Card,
+} from 'antd';
 import { TreeNodeNormal } from 'antd/lib/tree/Tree';
-import { Client, DefaultClientCodec, ClientContext } from '@hprose/rpc-core';
+import { Client } from '@hprose/rpc-core';
 import { Formatter } from '@hprose/io';
 import '@hprose/rpc-html5';
 import request from 'umi-request';
 
 import './index.css';
-import RadioGroup from 'antd/lib/radio/group';
-
-const { Title } = Typography;
 
 export default function() {
-  const [uri, setUri] = useState<string>('http://sstk.test/api');
+  const [uri, setUri] = useState<string>(localStorage.getItem("uri") || 'http://sstk.test/api');
   const [funcs, setFuncs] = useState<Array<TreeNodeNormal>>([
     {
       key: '0',
-      title: '等待载入中...',
+      title: '等待获取中...',
     },
   ]);
   const [funcLoading, setFuncLoading] = useState<boolean>(false);
+  const [paramsInput, setParamsInput] = useState<any>(localStorage.getItem("params") ?? `// JSON 格式 请求参数
+{} | []`);
   const [params, setParams] = useState<any>();
   const [currentFunction, setCurrentFunction] = useState<string>('');
   const [invokeResponse, setInvokeResponse] = useState<string>('// 响应结果输出...');
   const [invokeResultMode, setInvokeResultMode] = useState<string>('Normal');
   const [invokeResultTimeout, setInvokeResultTimeout] = useState<number>(300000);
+  const [invokeResponseLoading, setInvokeResponseLoading] = useState<boolean>(false);
 
   const handleConnectHprose = (e: any) => {
+    if(localStorage.getItem("params") === `// JSON 格式 请求参数
+{} | []`){
+      setParamsInput(`{
+  "":""
+}`);
+} else {
+  setParams(localStorage.getItem("params"));
+}
+
     request
       .post(uri, {
         data: 'z',
@@ -63,8 +81,9 @@ export default function() {
   };
 
   const getClient = (): Client => {
+    localStorage.setItem("uri", uri);
     let Settings = {
-      timeout: invokeResultTimeout
+      timeout: invokeResultTimeout,
     };
     const client = new Client(uri);
     client.useService<any>();
@@ -73,26 +92,31 @@ export default function() {
 
   const handleInvoke = () => {
     try {
+      localStorage.setItem("params", params);
+      setInvokeResponse('载入中...');
+      setInvokeResponseLoading(true);
       getClient()
-      .invoke(currentFunction, [JSON.parse(params)])
-      .then(response => {
-        if (response) {
-          setInvokeResponse(JSON.stringify(response, null, 2));
-        }
-      })
-      .catch(e => {
-        notification.warn({
-          message: '错误提示',
-          description: e.toString(),
+        .invoke(currentFunction, [JSON.parse(params)])
+        .then(response => {
+          if (response) {
+            setInvokeResponseLoading(false);
+            setInvokeResponse(JSON.stringify(response, null, 2));
+          }
+        })
+        .catch(e => {
+          notification.warn({
+            message: '错误提示',
+            description: e.toString(),
+          });
+          setInvokeResponseLoading(false);
         });
-      });
-    } catch(e) {
+    } catch (e) {
       notification.error({
-        message: "调用出错",
-        description: `请检请求参数是否正确...\n${e.toString()}`
+        message: '调用出错',
+        description: `请检请求参数是否正确...\n${e.toString()}`,
       });
+      setInvokeResponseLoading(false);
     }
-
   };
 
   const handleUriChange = (e: any) => {
@@ -108,24 +132,33 @@ export default function() {
       <Row gutter={{ xs: 8, sm: 16, md: 24 }}>
         <Col span={21}>
           <Input.Search
-            size='large'
+            size="large"
             placeholder="请输入Hprose服务接口地址..."
-            defaultValue="http://"
+            defaultValue={localStorage.getItem("uri") || "http://"}
             onChange={handleUriChange}
+            onPressEnter={handleConnectHprose}
           />
         </Col>
         <Col span={3}>
-          <Button type="primary" size='large' onClick={e => handleConnectHprose(e)}>
+          <Button type="primary" size="large" onClick={e => handleConnectHprose(e)}>
             获取方法列表
           </Button>
         </Col>
       </Row>
       <Row gutter={{ xs: 8, sm: 16, md: 24 }} style={{ marginTop: 20 }}>
         <Col span={6}>
-          <Card title="方法" style={{ height: '100%', minHeight: 650, maxHeight: 650, paddingBottom: 20 }}>
+          <Card
+            title={`方法 (${currentFunction || '空'})`}
+            style={{ height: '100%', minHeight: 650, maxHeight: 650, paddingBottom: 20 }}
+          >
             <Tree
               showLine
-              style={{height: 543, paddingBottom: 20, overflow: 'scroll'}}
+              style={{
+                height: 543,
+                paddingBottom: 20,
+                overflow: 'scroll',
+                fontSize: 15,
+              }}
               treeData={funcs}
               onSelect={(selectedKeys, info) => {
                 setCurrentFunction(selectedKeys[0]);
@@ -134,24 +167,26 @@ export default function() {
           </Card>
         </Col>
         <Col span={6}>
-          <Card title="请求" style={{ height: '100%', minHeight: 650, maxHeight: 650, paddingBottom: 20 }}>
+          <Card
+            title="请求"
+            style={{ height: '100%', minHeight: 650, maxHeight: 650, paddingBottom: 20 }}
+          >
             <CodeMirror
-              value='// JSON 格式 请求参数
-{} | []'
+              value={paramsInput}
               options={{
                 mode: 'javascript',
                 theme: 'material',
                 lineNumbers: true,
                 tabSize: 2,
                 readOnly: false,
-                hint: 'codeMirror.hint.auto'
+                hint: 'codeMirror.hint.auto',
               }}
               onChange={(editor, data, value) => {
                 setParams(value);
               }}
             />
             <Divider />
-            <Button type="primary" size="large" onClick={() => handleInvoke()}>
+            <Button type="primary" size="large" onClick={() => handleInvoke()} loading={invokeResponseLoading}>
               发送请求
             </Button>
           </Card>
@@ -160,8 +195,9 @@ export default function() {
           <Card
             title="响应"
             style={{ height: '100%', minHeight: 650, maxHeight: 650, paddingBottom: 20 }}
+            loading={invokeResponseLoading}
           >
-          <CodeMirror
+            <CodeMirror
               value={invokeResponse}
               options={{
                 mode: 'javascript',
@@ -171,7 +207,6 @@ export default function() {
             />
             <Divider />
           </Card>
-
         </Col>
       </Row>
     </div>
